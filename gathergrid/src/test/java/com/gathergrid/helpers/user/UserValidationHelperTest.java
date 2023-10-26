4ef232f0471b022c746f10a85b3cfe1f0217cf8d
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-10-26 14:42:58 +0100

    Add Documentation To Some Functions

--- gathergrid/src/test/java/com/gathergrid/helpers/user/UserValidationHelperTest.java
+++ gathergrid/src/test/java/com/gathergrid/helpers/user/UserValidationHelperTest.java
@@ -20,6 +20,7 @@ import java.util.stream.Stream;
 class UserValidationHelperTest {
 
     UserRepository userRepository;
+    
     UserValidationHelper userValidationHelper;
 
     @BeforeEach
@@ -31,6 +32,10 @@ class UserValidationHelperTest {
     @Nested
     class NoUserHasThisEmail {
 
+        /**
+         * A test to verify that "NoUserHasThisEmail" returns false when the specified email already exists.
+         *
+         */
         @Test
         public void khasReturnFalseWhenEmailExists() {
             when(userRepository.existsByEmail("saadmeddiche2004201@gmail.com")).thenReturn(true);
@@ -40,6 +45,11 @@ class UserValidationHelperTest {
             assertFalse(result); 
         }
 
+        /**
+         * A test case to verify that the method "noUserHasThisEmail" returns true
+         * when the email does not exist in the user repository.
+         *
+         */
         @Test
         public void khasReturnTrueWhenEmailDoesNotExist() {
             when(userRepository.existsByEmail("notARealEmail@gmail.com")).thenReturn(false);
@@ -53,6 +63,11 @@ class UserValidationHelperTest {
     @Nested
     class PasswordsAreNotMatched {
 
+        /**
+         * Test case to verify that the 'passwordsAreNotMatched' function returns false when the given password
+         * matches the fetched password.
+         *
+         */
         @Test
         public void khasReturnFalseWhenPasswordsMatch() {
             String givenPassword = "password#2004";
@@ -63,6 +78,11 @@ class UserValidationHelperTest {
             assertFalse(result);
         }
 
+        /**
+         * A test case to verify that the method 'passwordsAreNotMatched' returns true
+         * when the given password and the fetched password do not match.
+         *
+         */
         @Test
         public void khasReturnTrueWhenPasswordsDoNotMatch() {
             String givenPassword = "password#2004";
@@ -73,6 +93,10 @@ class UserValidationHelperTest {
             assertTrue(result);
         }
 
+        /**
+         * Test case to verify that the method `passwordsAreNotMatched` returns
+         * `true` when given password is null.
+         */
         @Test
         public void khasReturnTrueWhenGivenPasswordIsNull() {
             String givenPassword = null;
@@ -83,6 +107,11 @@ class UserValidationHelperTest {
             assertTrue(result);
         }
 
+        /**
+         * Test case to verify if the method `passwordsAreNotMatched` returns `true`
+         * when the fetched password is null.
+         *
+         */
         @Test
         public void khasReturnTrueWhenFetchedPasswordIsNull() {
             String givenPassword = "password#2004";
@@ -93,6 +122,11 @@ class UserValidationHelperTest {
             assertTrue(result);
         }
 
+        /**
+         * A test case to verify that the method 'passwordsAreNotMatched' returns true 
+         * when the given password is empty and the fetched password is not empty.
+         *
+         */
         @Test
         public void khasReturnTrueWhenPasswordsAreEmpty() {
             String givenPassword = "";
@@ -107,6 +141,10 @@ class UserValidationHelperTest {
     @Nested
     class validateUser {
 
+        /**
+         * A test case to validate that the function throws an AlreadyExistsException
+         * when the email already exists in the user repository.
+         */
         @Test
         public void khasThrowAlreadyExistsExceptionWhenEmailAlreadyExists() {
             User user = new User("Saad", "Meddiche", "SaadOun", "saadmeddiche2004201@gmail.com", "Password#2004");
@@ -118,6 +156,10 @@ class UserValidationHelperTest {
             });
         }
 
+        /**
+         * Test method to verify that the function throws an AlreadyExistsException
+         * when the username already exists.
+         */
         @Test
         public void khasThrowAlreadyExistsExceptionWhenUserNameAlreadyExists() {
             User user = new User("Saad", "Meddiche", "SaadOun", "saadmeddiche2004201@gmail.com", "Password#2004");
@@ -129,6 +171,9 @@ class UserValidationHelperTest {
             });
         }
 
+        /**
+         * Test case to verify that the function does not throw any exception when all conditions are good.
+         */
         @Test
         public void khasNotThrowExceptionWhenAllIsGood() {
             User user = new User("Saad", "Meddiche", "SaadOun", "saadmeddiche2004201@gmail.com", "Password#2004");
@@ -140,17 +185,10 @@ class UserValidationHelperTest {
             });
         }
 
-        @Test
-        public void khasThrowValidationExceptionWhen() {
-            User user = new User("Saad", "Meddiche", null, "saadmeddiche2004201@gmail.com", "Password#2004");
-            when(userRepository.existsByEmail(user.getEmail().getAddressEmail())).thenReturn(false);
-            when(userRepository.existsByUsername(user.getName().getUserName())).thenReturn(false);
-
-            assertThrows(ValidationException.class, () -> {
-                userValidationHelper.validateUser(user);
-            });
-        }
-
+        /**
+         * Generates a stream of dynamic tests to validate that the function throws a ValidationException
+         * when some attributes of the User object are null.
+         */
         @TestFactory
         public Stream<DynamicTest> khasThrowValidationExceptionWhenSomeAttributesAreNull() {
 
